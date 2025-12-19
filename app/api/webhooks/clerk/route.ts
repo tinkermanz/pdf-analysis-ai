d946bb4d5ad4a6b13c19b18292c8a9d478b74541
tinkermanz <m.b.istiak@outlook.com>
2025-12-19 19:48:47 +0600

    test: clerk webhook (successfully)

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,20 +1,31 @@
+import prisma from "@/lib/prisma";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
 	try {
+		const webhookSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+		if (!webhookSecret) {
+			throw new Error(
+				"Error: Please add WEBHOOK_SECRET from cleark dashboard to .env"
+			);
+		}
 		const evt = await verifyWebhook(req);
 
-		// Do something with payload
-		// For this guide, log payload to console
-		// const { id } = evt.data;
 		const eventType = evt.type;
-
 		if (eventType === "user.created") {
-			const { id, email, address, first_name, last_name } = evt.data;
+			const { id, email_addresses, first_name, last_name } = evt.data;
 
 			const fullName = `${first_name || ""} ${last_name || ""}`.trim();
 			// TODO: call prisma to create an user
+
+			await prisma.user.create({
+				data: {
+					id,
+					email: email_addresses[0].email_address,
+					name: fullName || null,
+				},
+			});
 		}
 		return new Response("Webhook received", { status: 200 });
 	} catch (err) {
